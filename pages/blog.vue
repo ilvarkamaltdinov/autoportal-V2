c602e94671c6be86f1e1eb657277cc0c41db1591
rxMATTEO <erik_sundberg@mail.ru>
2023-12-11 14:17:42 +0300

    Add blog

---
+++ pages/blog.vue
@@ -0,0 +1,13 @@
+<template>
+ <main class="page-main">
+  <div class="grid grid--container">
+    <section class="page-main__blog blog grid">
+      <Blog is-index/>
+    </section>
+  </div>
+ </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
