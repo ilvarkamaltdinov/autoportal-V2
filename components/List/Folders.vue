d729c6e5654560196163bbcd523d668ffe01ecc8
ilvar <ilvar@u2c.su>
2023-12-12 11:42:12 +0300

    весртка

---
+++ components/List/Folders.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="grid__col-12">
+    <div class="grid__col-12">
+      <div class="tabs">
+        <ul class="tabs__list tabs__list--all tabs__list--all-models">
+          <li role="presentation" class="tabs__item" v-for="folder in currentFolders" :key="folder.id">
+            <a href="/used/land-rover/range-rover-sport"
+               class="tabs__link">
+              <span class="tabs__text">{{ folder.title }}</span>
+              <span class="tabs__count">{{ folder.offers_count }}</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useSiteConfig} from '~/store/siteConfig';
+import {Folder} from '~/app/types/folders';
+
+const route = useRoute();
+const marks = useSiteConfig().marks;
+
+const currentFolders = computed<Folder[]>(() => {
+  let currentMark = marks.find(mark => mark.slug === route.params.mark);
+  if (currentMark) {
+    return currentMark.folders;
+  }
+
+});
+</script>
