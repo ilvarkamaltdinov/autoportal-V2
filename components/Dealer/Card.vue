c0ab321e2e0c7d693cc8d64ba04d73c70efe85bd
rxMATTEO <erik_sundberg@mail.ru>
2023-12-15 17:25:31 +0300

    Change dealer card

--- components/Dealer/Card.vue
+++ components/Dealer/Card.vue
@@ -3,69 +3,23 @@
     <div class="dealers__features features">
       <div class="heading-group">
         <div class="heading-group__wrap">
-          <h2 class="heading heading--h2">{{ dealer.title }}</h2>
-          <span class="heading-group__label">{{ dealer.short_description }}</span>
+          <h2 class="heading heading--h2">
+            <slot name="header"/>
+          </h2>
+          <span class="heading-group__label">
+            <slot name="short-description"/>
+          </span>
         </div>
-        <rating v-tippy="{
-                content:`<div class='tippy__text'>Рейтинг дилера</div>`,
-                animation:'scale',
-                arrow: true,
-            }"
-                :rating="dealer.rating"
-                :max="5"/>
+        <slot name="right-corner"/>
       </div>
-      <div class="features__group">
-        <h3 class="heading heading--h3">Адрес:</h3>
-        <ul class="features__list">
-          <li class="features__item">{{ dealer.address }}</li>
-          <li v-if="dealer.metro"
-              class="features__item">м. {{ dealer.metro }}
-          </li>
-        </ul>
-      </div>
-      <div class="features__group" v-if="dealer.phone">
-        <h3 class="heading heading--h3">Телефон:</h3>
-        <ul class="features__list">
-          <li class="features__item">
-            <a :href="`tel:${ dealer.phone.replace(/[^+\d]/g, '')}`"> {{ dealer.phone }}</a>
-          </li>
-        </ul>
-      </div>
-      <div class="features__group">
-        <h3 class="heading heading--h3">Режим работы:</h3>
-        <ul class="features__list">
-          <li class="features__item">{{ dealer.schedule }}</li>
-        </ul>
-      </div>
-      <button v-if="dealer.slug === $route.params.dealer"
-                      @click="showMore(dealer.slug)"
-                      class="button--show">
-        Подробнее о дилере
-      </button>
-      <nuxt-link v-else
-                      :href="`/contact/${dealer.slug}`"
-                      class="button--show">
-        Подробнее о дилере
-      </nuxt-link>
-      <a v-if="dealer.site"
-         class="button button--show button--show-link"
-         :href="`${dealer.site}`"
-         target="_blank">Сайт автоцентра
-      </a>
+      <slot name="content"/>
     </div>
-    <div class="dealers__item-img-wrap">
-      <NuxtImg
-          v-if="dealer.images[0]"
-          class="dealers__item-img lazyload"
-          :src="dealer.images[0].small"
-          format="webp"
-          densites="1x 2x"/>
-    </div>
+    <slot name="image"/>
   </div>
 </template>
 
 <script setup lang="ts">
-import {Dealer} from '~/types/graphql';
+import { Dealer } from '~/types/graphql';
 
 defineProps<{
   dealer: Dealer
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,31 +1,106 @@
 <template>
   <main class="page-main">
     <div class="grid">
-<!--      <crumbs :crumbs="crumbs" />-->
+      <!--      <crumbs :crumbs="crumbs" />-->
     </div>
     <div class="grid grid--container">
-        <section class="page-main__dealers dealers grid">
-          <div class="heading-group heading-group--h1">
-            <div class="heading-group__wrap">
-              <h1 class="heading heading--h1">
-                {{ title }}
-                <span class="heading__promo"></span>
-              </h1>
-              <span class="heading-group__label">Партнеры carro</span>
-            </div>
+      <section class="page-main__dealers dealers grid">
+        <div class="heading-group heading-group--h1">
+          <div class="heading-group__wrap">
+            <h1 class="heading heading--h1">
+              {{ title }}
+              <span class="heading__promo"></span>
+            </h1>
+            <span class="heading-group__label">Партнеры carro</span>
           </div>
-          <Card v-for="dealer in dealers" :key="dealer.id" :dealer="dealer" />
-        </section>
+        </div>
+        <Card v-for="dealer in dealers" :key="dealer.id" :dealer="dealer">
+          <template #header>
+            {{ dealer.title }}
+          </template>
+          <template #short-description>
+            {{ dealer.short_description }}
+          </template>
+          <template #right-corner>
+            <Rating v-tippy="{
+              content:`<div class='tippy__text'>Рейтинг дилера</div>`,
+              animation:'scale',
+              arrow: true,
+              }"
+                    :rating="dealer.rating"
+                    :max="5"/>
+          </template>
+          <template #content>
+            <div class="features__group" v-for="feature in contactItems(dealer)" :key="feature.title">
+              <h3 class="heading heading--h3">{{ feature.title }}</h3>
+              <ul class="features__list">
+                <li class="features__item" v-for="item in feature.items" :key="item">
+                  {{ item }}
+                </li>
+              </ul>
+            </div>
+            <button v-if="dealer.slug === $route.params.dealer"
+                    @click="showMore(dealer.slug)"
+                    class="button--show">
+              Подробнее о дилере
+            </button>
+            <nuxt-link v-else
+                       :href="`/contact/${dealer.slug}`"
+                       class="button--show">
+              Подробнее о дилере
+            </nuxt-link>
+            <a v-if="dealer.site"
+               class="button button--show button--show-link"
+               :href="`${dealer.site}`"
+               target="_blank">Сайт автоцентра
+            </a>
+          </template>
+          <template #image>
+            <div class="dealers__item-img-wrap">
+              <NuxtImg
+                  v-if="dealer.images[0]"
+                  class="dealers__item-img lazyload"
+                  :src="dealer.images[0].small"
+                  format="webp"
+                  densites="1x 2x"/>
+            </div>
+          </template>
+        </Card>
+      </section>
     </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import {useDealers} from '~/store/dealers';
-import {storeToRefs} from 'pinia';
+import { useDealers } from '~/store/dealers';
+import { storeToRefs } from 'pinia';
 import Card from '~/components/Dealer/Card.vue';
+import { Dealer } from '~/types/graphql';
 
 const dealersStore = useDealers();
 dealersStore.fetchDealers();
-const {dealers} = storeToRefs(dealersStore);
+const { dealers } = storeToRefs(dealersStore);
+
+//todo add <a> to redirect to dealer
+const contactItems = (dealer: Dealer) => [
+  {
+    title: 'Адрес',
+    items: [
+      dealer.address,
+      dealer.metro,
+    ]
+  },
+  {
+    title: 'Телефон',
+    items: [
+      dealer.phone
+    ]
+  },
+  {
+    title: 'Режим работы',
+    items: [
+      dealer.schedule
+    ]
+  },
+];
 </script>
