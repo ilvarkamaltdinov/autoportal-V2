b17d196c5d846e03f038e36e1a818759cc657631
rxMATTEO <erik_sundberg@mail.ru>
2023-12-15 10:14:10 +0300

    Change articles, add dom parser

--- pages/blog/ratings/[article].vue
+++ pages/blog/ratings/[article].vue
@@ -1,7 +1,7 @@
 <template>
   <main class="page-main">
     <div class="grid grid--container">
-      <section class="page-main__blog blog grid">
+      <section class="page-main__blog blog grid" v-if="article">
         <div class="heading-group heading-group--h1">
           <div class="heading-group__wrap">
             <h1 class="heading heading--h1">{{ article.page_title }}</h1>
@@ -17,23 +17,19 @@
             <div class="text__wrap grid__col-12">
               <div class="text__content"
                    v-if="article.body"
-                   v-html="(article.body)">
+                   v-html="domParse(article.body)">
               </div>
               <div class="text__content"
                    v-if="article.image">
                 <div class="text__img-wrap">
-                  <picture-component classes="text__img text__img--blog lazyload"
-                                     :small="article.image.medium"
-                                     :small-webp="article.image.medium_webp"
-                                     :big="article.image.src"
-                                     :big-webp="article.image.src"/>
+                  <NuxtImg :src="article.image.medium" class="text__img text__img--blog" format="webp"/>
                 </div>
               </div>
-              <blog-video v-if="article.video_youtube"
-                          :id="article.video_youtube"/>
+<!--              <blog-video v-if="article.video_youtube"-->
+<!--                          :id="article.video_youtube"/>-->
 
-              <blog-slider v-if="article.image_slide && article.image_slide.length"
-                           :images="article.image_slide"/>
+<!--              <blog-slider v-if="article.image_slide && article.image_slide.length"-->
+<!--                           :images="article.image_slide"/>-->
 
             </div>
           </div>
@@ -47,9 +43,39 @@
 import {Article, ArticleQueryVariables} from '~/types/graphql';
 import {request} from '~/helpers/request';
 import {article as articleQuery} from '~/apollo/queries/blog/article';
+import DOMParser from 'universal-dom-parser';
 
 const article = ref<Article | null>(null);
 article.value = (await request<{ article: Article }, ArticleQueryVariables>(articleQuery, {
   url: useRoute().path,
 })).data.value.article;
+
+function addClass(arr, className) {
+  arr.forEach(value => {
+    if (value.innerHTML === '&nbsp;') {
+      value.remove();
+    }
+    value.classList.add(className);
+  });
+}
+
+function domParse(str: string) {
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(`${str}`, 'text/html');
+
+  let p_array = doc.documentElement.querySelectorAll('p');
+  let h2_array = doc.documentElement.querySelectorAll('h2');
+  let ul_array = doc.documentElement.querySelectorAll('ul');
+  let ol_array = doc.documentElement.querySelectorAll('ol');
+  let li_array = doc.documentElement.querySelectorAll('li');
+
+  addClass(p_array, 'text__p');
+  addClass(h2_array, 'heading--h2');
+  addClass(h2_array, 'heading');
+  addClass(ul_array, 'text__list');
+  addClass(ol_array, 'text__list');
+  addClass(li_array, 'text__list-item');
+
+  return doc.documentElement.outerHTML;
+}
 </script>
