41328ab5187e93146783b638b643fdd8fa5d5e61
rxMATTEO <erik_sundberg@mail.ru>
2024-01-22 18:07:32 +0300

    Change rassro4ka

--- pages/rassrochka.vue
+++ pages/rassrochka.vue
@@ -1,9 +1,9 @@
 <template>
-	<main class="page-main">
-		<div class="grid">
-<!--			<crumbs :crumbs="crumbs" />-->
-		</div>
-		<div class="grid grid--container">
+  <main class="page-main">
+    <div class="grid">
+      <!--			<crumbs :crumbs="crumbs" />-->
+    </div>
+    <div class="grid grid--container">
       <section class="page-main__application application grid">
         <div class="heading-group heading-group--h1">
           <div class="heading-group__wrap">
@@ -13,31 +13,56 @@
         </div>
         <div class="grid__col-12 grid grid--application">
           <div class="application__form grid__col-4">
-<!--            <form-installment :bank-rate="bank.rate" />-->
+            <div class="heading-group heading-group--form">
+              <div class="heading-group__wrap">
+                <h2 class="heading heading--h2">
+                  Рассрочка
+                  <span class="heading__promo">0%
+          </span>
+                </h2>
+              </div>
+            </div>
+            <form class="form">
+              <fieldset class="form__fieldset">
+                <label
+                    class="form__field-wrap form__field-wrap--car ">
+                  <Button class="form__field" @click.prevent="choseCar()">
+                    {{ 'Выбрать автомобиль' }}
+                    <span v-if="currentCar">
+                      {{ currentCar.price | toCurrency }}
+                    </span>
+                  </Button>
+                  <svg-icon name="icon-form" class="form__car-icon"/>
+                </label>
+              </fieldset>
+              <div class="catalog form__catalog" v-if="$device.isMobile && (offer || currentCar)">
+                <catalog-item-large-mobile-form :is-form="true" :offer="offer || currentCar"/>
+              </div>
+              <form-credit-calculator installment @changePeriod="changePeriod" @changePayment="changePayment"
+                                      :params="creditParams" :offer="offer || currentCar"/>
+              <fieldset class="form__fieldset">
+                <label class="form__field-wrap" :class="nameClass">
+                  <inputs-input placeholder="ФИО" @input="handlerInput('name')" v-model="form.name.value" type="text"/>
+                </label>
+                <label class="form__field-wrap" :class="phoneClass">
+                  <inputs-input placeholder="Телефон" @input="handlerInput('phone')"
+                                @phoneMaskComplete="form.phone.valid = true"
+                                @onincomplete="form.phone.valid = null" v-model="form.phone.value" mask="phone"
+                                type="tel"/>
+                </label>
+                <checkbox-form :error="error === 'agreeRf'" @change="changeCheckbox($event, 'agreeRf')"
+                               label="Подтверждаю наличие гражданства РФ"/>
+                <checkbox-form :error="error === 'agree'" @change="changeCheckbox($event, 'agree')" label="Согласен на"
+                               link="обработку личных данных"/>
+              </fieldset>
+              <button-typical :loading="buttonDisabled" text="Оставить заявку"
+                              button-class="button--credit button--form"/>
+            </form>
           </div>
-<!--          <div v-if="currentCar && !$device.isMobile"-->
-<!--               class="application__catalog catalog grid__col-8">-->
-<!--            <component :choose="true" :is="component"-->
-<!--                       :has-fancy="true"-->
-<!--                       :key="currentCar.id"-->
-<!--                       :is-form="true"-->
-<!--                       :has-buttons="false"-->
-<!--                       :offer="currentCar" />-->
-<!--          </div>-->
-<!--          <button v-else-->
-<!--                  class="application__choose-car grid__col-8"-->
-<!--                  @click="chooseCar">-->
-<!--            <nuxt-icon class="application__choose-car-icon"-->
-<!--                      name="icon-form" />-->
-<!--            <span class="application__choose-car-text">Выберите автомобиль</span>-->
-<!--          </button>-->
-<!--          <application-banks default-bank="sovcom"-->
-<!--                             class="grid__col-5" />-->
-<!--          <application-terms installment class="grid__col-3" />-->
         </div>
       </section>
-		</div>
-	</main>
+    </div>
+  </main>
 </template>
 <script setup lang="ts">
 
