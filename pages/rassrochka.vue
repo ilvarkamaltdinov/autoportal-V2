ec3af9a58fc4baae996034fb6f6d5a5a36a327a5
rxMATTEO <erik_sundberg@mail.ru>
2024-01-24 17:55:36 +0300

    Change installment so its done

--- pages/rassrochka.vue
+++ pages/rassrochka.vue
@@ -44,11 +44,46 @@
         </div>
       </section>
       <div class="benefits grid__col-12">
-      <Benefits type="installment" />
+        <Benefits type="installment"/>
       </div>
+      <section class="page-main__text text grid__col-12">
+        <h2 class="heading heading--h2">Об услуге</h2>
+        <div class="grid">
+          <ContentBlock class="grid__col-12">
+            <template #content>
+              <p class="text__p">
+                На CARRO вы можете приобрести автомобиль в рассрочку без любых переплат и процентов.
+              </p>
+              <p class="text__p">
+                Рассрочка является удобным способом покупки транспортного средства с пробегом, учитывая,
+                что процентная ставка не начисляется. Рассрочка станет идеальным решением в случае
+                погашения стоимости автомобиля за срок до 3 лет. Если же выплата всей суммы за 3 года
+                обременительна для семейного бюджета, оптимальным решением станет
+                <nuxt-link to="/credit">автокредит</nuxt-link>
+                {{ $settings.credit_percent }} со скидкой {{ $settings.sale_credit }}.
+              </p>
+              <p class="text__p">
+                Окончательное решение о предоставлении рассрочки принимается банками-партнерами CARRO.
+              </p>
+              <p class="text__p">
+                Преимущества рассрочки carro.ru:
+              </p>
+              <ul class="text__list">
+                <li class="text__list-item">Ставка {{ $settings.first_installment }}</li>
+                <li class="text__list-item">Без первоначального взноса</li>
+                <li class="text__list-item">Комплект резины в подарок</li>
+              </ul>
+            </template>
+            <template #figure>
+              <NuxtImg class="text__figure-img" src="figures/figure-2@2x.webp" />
+            </template>
+          </ContentBlock>
+        </div>
+      </section>
     </div>
   </main>
 </template>
 <script setup lang="ts">
 import ApplicationBankCard from '~/components/Application/ApplicationBankCard.vue';
+import ContentBlock from '~/components/TextContent/ContentBlock.vue';
 </script>
