dab85a40d178942ce65abe50285654b995c2b57d
ilvar <ilvar@u2c.su>
2024-01-25 12:39:05 +0300

    весртка

--- components/Catalog/Item/Image.vue
+++ components/Catalog/Item/Image.vue
@@ -5,10 +5,16 @@
 <script setup lang="ts">
 import ImageHoverSlider from '~/components/Catalog/Item/ImageHoverSlider.vue';
 import ImageNativeSlider from '~/components/Catalog/Item/ImageNativeSlider.vue';
+import ImageLongSlider from '~/components/Catalog/Item/ImageLongSlider.vue';
 
-const {isMobile} = useDevice();
+
+const props = defineProps<{
+  view?: string
+}>();
+
+const { isMobile } = useDevice();
 const imageComponent = computed(() => {
-  return isMobile ? ImageNativeSlider : ImageHoverSlider;
+  return isMobile ? ImageNativeSlider : props.view === 'long' ? ImageLongSlider : ImageHoverSlider;
 });
 </script>
 
--- components/Catalog/Item/ImageNativeSlider.vue
+++ components/Catalog/Item/ImageNativeSlider.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="catalog__img">
-    <a v-for="i in 3" :key="i" href="#" class="catalog__img-link" title="Подробнее об автомобиле"
-       aria-label="Подробнее об автомобиле">
-      <img
-          data-src="/img/mock/offer-card-img-1.webp" alt="image"
-          class=" lazyloaded"
-          src="/img/mock/offer-card-img-1.webp"/>
-    </a>
-    
-    <div>
-      <div class="catalog__call-card">
-        <div class="catalog__call">
-          <nuxt-icon name="icon-call" class="catalog__call-icon icon sprite-icons"/>
-          <span class="catalog__call-title">Бесплатный звонок</span>
-          <span class="catalog__call-dealer">Автоцентр АвтоГрадъ</span>
-        </div>
-        <div class="catalog__call-img">
-          <img data-src="https://riadvance.s3mts.ru/images/items/b61/61bf45a4b/22689c3c35f2.webp" class="lazyload">
-        </div>
+  <a v-for="i in 3" :key="i" href="#" class="catalog__img-link" title="Подробнее об автомобиле"
+     aria-label="Подробнее об автомобиле">
+    <img
+        data-src="/img/mock/offer-card-img-1.webp" alt="image"
+        class=" lazyloaded"
+        src="/img/mock/offer-card-img-1.webp"/>
+  </a>
+  <div>
+    <div class="catalog__call-card">
+      <div class="catalog__call">
+        <nuxt-icon name="icon-call" class="catalog__call-icon icon sprite-icons"/>
+        <span class="catalog__call-title">Бесплатный звонок</span>
+        <span class="catalog__call-dealer">Автоцентр АвтоГрадъ</span>
+      </div>
+      <div class="catalog__call-img">
+        <img data-src="https://riadvance.s3mts.ru/images/items/b61/61bf45a4b/22689c3c35f2.webp" class="lazyload" alt=""
+             src="">
       </div>
     </div>
   </div>
