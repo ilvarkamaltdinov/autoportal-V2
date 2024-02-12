9d4e7da8154db6d54ef42257b4b6f281b64635a6
ilvar <ilvar@u2c.su>
2024-02-12 15:25:05 +0300

    верстка

--- components/Filters/List.vue
+++ components/Filters/List.vue
@@ -1,11 +1,14 @@
 <template>
   <ul class="filter__menu-list">
     <li>
-      <ControlsSelect @select="handlerSelect" :options="notEmptyMarks" title="Марка" type="mark" :value="mark"/>
+      <Dropdown optionLabel="title" v-model="mark" :options="notEmptyMarks" placeholder="Марка"/>
     </li>
     <li>
-      <ControlsSelect @select="handlerSelect" :options="folders" title="Модель" type="folder" :value="folder"/>
+      <Dropdown optionLabel="title" v-model="folder" :options="folders" placeholder="Модель"/>
     </li>
+    <!--    <li>-->
+    <!--      <ControlsSelect @select="handlerSelect" :options="folders" title="Модель" type="folder" :value="folder"/>-->
+    <!--    </li>-->
     <Accordion v-model:active-index="accordionIndex" :unstyled="true">
       <template #collapseicon>
         {{ null }}
@@ -16,19 +19,19 @@
       <AccordionTab :unstyled="true">
         <ul class="filter__menu-list filter__menu-list--more">
           <li>
-            <ControlsSelect options="" title="Поколение" type="" value=""/>
+            <Dropdown optionLabel="name" v-model="generation" :options="generations" placeholder="Поколение"/>
           </li>
           <li>
-            <ControlsSelect options="" title="Двигатель" type="" value=""/>
+            <Dropdown optionLabel="title" v-model="engineType" :options="engineTypes" placeholder="Двигатель"/>
           </li>
           <li>
-            <ControlsSelect options="" title="Кузов" type="" value=""/>
+            <Dropdown optionLabel="title" v-model="bodyType" :options="bodyTypes" placeholder="Кузов"/>
           </li>
           <li>
-            <ControlsSelect options="" title="КПП" type="" value=""/>
+            <Dropdown optionLabel="title" v-model="gearbox" :options="gearboxes" placeholder="КПП"/>
           </li>
           <li>
-            <ControlsSelect options="" title="Привод" type="" value=""/>
+            <Dropdown optionLabel="title" v-model="driveType" :options="driveTypes" placeholder="Привод"/>
           </li>
         </ul>
       </AccordionTab>
@@ -52,13 +55,12 @@
 </template>
 
 <script setup lang="ts">
-import ControlsSelect from '~/components/Controls/Select.vue';
 import RangeFilterPrice from '~/components/Range/FilterPrice.vue';
 import { useMarks } from '~/store/carbrandsStore';
 import { Mark } from '~/app/types/marks';
 import { Folder } from '~/app/types/folders';
 import { storeToRefs } from 'pinia';
-
+import { Generation } from '~/types/graphql';
 
 const allFilters = ref<boolean>(false);
 const accordionIndex = computed(() => allFilters.value ? 0 : -1);
@@ -66,8 +68,14 @@ const router = useRouter();
 const marksStore = useMarks();
 marksStore.getAllMarksFillPopular();
 const { allMarks } = storeToRefs(marksStore);
+
 const mark = ref<Mark | null>();
 const folder = ref<Folder | null>();
+const generation = ref<Generation | null>();
+const engineType = ref();
+const bodyType = ref();
+const driveType = ref();
+const gearbox = ref();
 
 const notEmptyMarks = computed(() => {
   return allMarks.value.filter(item => item.offers_count > 0);
@@ -75,6 +83,26 @@ const notEmptyMarks = computed(() => {
 
 const folders = computed(() => {
   return mark.value?.folders.filter(item => item.offers_count > 0) || [];
+});
+
+const generations = computed(() => {
+  return folder.value?.generations || [];
+});
+
+const engineTypes = computed(() => {
+  return [];
+});
+
+const bodyTypes = computed(() => {
+  return [];
+});
+
+const driveTypes = computed(() => {
+  return [];
+});
+
+const gearboxes = computed(() => {
+  return [];
 });
 
 async function handlerSelect(data: { option: object, type: string }) {
